87045f1a7b7a8f24da978afd5390ee0053552eee
Norbory <a20162575@pucp.edu.pe>
2025-06-03 00:38:25 -0500

    Feat: Cambiar valor de information version

--- 01. CSharp/obj/Debug/net8.0/01. CSharp.AssemblyInfo.cs
+++ 01. CSharp/obj/Debug/net8.0/01. CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6620b82b20a4c614b6dcd5a4e8201aec6c349a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42d6b9de7710b7caf4ba7786ccd01ffb443c39c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
